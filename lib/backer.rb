bd6fb88c9cf53104320fd58c60a495d0bd5f1b32
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-21 23:57:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,7 @@ class Backer
   
   def back_project(title)
     @backed_projects << title
+    # utilize the passed in title from other class to call on its backers method to then shovel the current instance (self) into it
     title.backers << self
   end
   
